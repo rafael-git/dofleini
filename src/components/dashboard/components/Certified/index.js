26b81b7fd1dd5ea4ba6364026b58d58117a99bcf
rafael-git <57331666+rafael-git@users.noreply.github.com>
2023-03-31 15:52:54 -0400

    Fix Certified

--- src/components/dashboard/components/Certified/index.js
+++ src/components/dashboard/components/Certified/index.js
@@ -8,29 +8,35 @@ import './index.css';
 const Certified = (props) => {
     const{ certified } = props;
     return (
-        <div className='certified'>
-            <img  className='img-certified' src={modwell} alt='text'/>
-            <div className='fifth-circle'>
-                <div className='fourth-circle'>
-                    <div className='third-circle'>
-                        <div className='second-circle'>
-                            <div className='inner-circle'>
-                                <div className='certified-content'>
-                                    <div className='certified-title'>
-                                        {certified.title}
+        <div className='certified-description'>
+            <div className='certified'>
+                <img  className='img-certified' src={modwell} alt='text'/>
+                <div className='fifth-circle'>
+                    <div className='fourth-circle'>
+                        <div className='third-circle'>
+                            <div className='second-circle'>
+                                <div className='inner-circle'>
+                                    <div className='certified-content'>
+                                        <div className='certified-title'>
+                                            {certified.title}
+                                        </div>
+                                        <div className='certified-value'>
+                                            {certified.certified ? 'certified' : 'not certified'}
+                                        </div>
+                                        {certified.certified ? <img className='img-check' src={ok} alt='check'/> : null}
                                     </div>
-                                    <div className='certified-value'>
-                                        {certified.certified ? 'certified' : 'not certified'}
-                                    </div>
-                                    {certified.certified ? <img className='img-check' src={ok} alt='check'/> : null}
+                                    
                                 </div>
-                                
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div className='description'>
+                <span className='description-text'>{certified.description}</span>
+            </div>
         </div>
+        
         
     );
 };
--- src/redux/dashboard/certifiedSlice.js
+++ src/redux/dashboard/certifiedSlice.js
@@ -5,25 +5,25 @@ const initialState = [
         title: '3D Desing',
         certified: true,
         icon: '',
-        description:''
+        description:'Become a Modwell 3D Design Specialists and help your clients navigate the outdated market'
     },
     {
         title: 'Home Vault',
         certified: true,
         icon: '',
-        description:''
+        description:'Become a Modwell Home Vault Specialists and help your clients manage their Largest Asset'
     },
     {
         title: 'Metaverse',
         certified: true,
         icon: '',
-        description:''
+        description:'Become a Modwell Meta Agent and help your clients navigate the metaverse'
     },
     {
         title: 'Digital IP',
         certified: false,
         icon: '',
-        description:''
+        description:'Become a Modwell Digital IP Specialists and help your clients manage their Intellectual Property'
     },
          
 ];
